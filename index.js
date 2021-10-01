59d3f44f8a8f12924c53ceb968b61b1825ffa45a
Richo-Dev <81697193+AdiRaihan46@users.noreply.github.com>
2021-10-01 11:32:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://nama.glitch.me"]
+const urls = ["https://nama.glitch.me"] //ubah sesuai nama yg akan di uptime!
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
